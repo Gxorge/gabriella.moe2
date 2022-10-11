32917c8261c10a94d41eca921b4c34949e4486e2
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-10-11 20:31:51 +0100

    Fixed links for rebrand

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,9 +39,9 @@
         <p id="title">Hello lovely!</p>
         <p id="text">I'm {nData.short}, welcome to my corner of the internet!</p>
         <p id="text">
-            <a href="https://github.com/IsGabriellaCurious">github</a>
+            <a href="https://github.com/Gxorge">github</a>
             <a href="https://scoresaber.com/u/76561198166602152">scoresaber</a>
-            <a href="mailto:isgabriellacurious@gmail.com">contact</a>
+            <a href="mailto:hello@ghot.mee">contact</a>
         </p>
         
     </div>
